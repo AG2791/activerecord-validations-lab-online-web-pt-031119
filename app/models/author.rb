08f6d90f3d0a1b45f23f1d4e7d00cb230665371b
AJ Guiles <ashiguiles@gmail.com>
2019-08-06 02:45:45 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,7 @@ class Author < ActiveRecord::Base
   validates :phone_number, length: { is: 10 }
   validates: :title, presence: true
   validates (:post_content, { :length => { :minimum => 250 } })
+  validates(:post_summary, { :length => { :maximum => 250 } })
 
 end
 
