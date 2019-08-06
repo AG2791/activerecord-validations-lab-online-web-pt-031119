c0d23bf4659d3394057d82b8fd230b0f077d6292
AJ Guiles <ashiguiles@gmail.com>
2019-08-06 02:49:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
    validates (:post_content, { :length => { :minimum => 250 } })
   validates(:post_summary, { :length => { :maximum => 250 } })
+  validates: :title, presence: true
 end
